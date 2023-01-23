c6eb748f4846f598f570e9ff55a9bf6557d77400
mohan <mohanr3054@gmail.com>
2023-01-23 18:23:48 +0530

    cors link added

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.get("/",(req,res)=>{
 
 app.use(express.json());
 app.use(cookiParser());
-app.use(cors("http://localhost:3000"));
+app.use(cors("https://cheerful-longma-32ba37.netlify.app"));
 app.use(router);
 
 
